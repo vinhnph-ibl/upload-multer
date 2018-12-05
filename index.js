5971ba4b7cb54bb6311ade3394b1a04ffdc51bb0
LT246-VINHNPH\vinhnph <vinhnph@blockpass.org>
2018-12-05 10:37:57 +0700

    read file

--- index.js
+++ index.js
@@ -1,14 +1,70 @@
 import 'dotenv/config'
 import express from 'express'
 import multer from 'multer'
-var upload = multer({ dest: 'uploads/' })
+import path from 'path'
+import fs from 'fs'
+import readChunk from 'read-chunk'
+import fileType from 'file-type'
 
 const app = express()
+const UPLOAD_PATH = path.join(__dirname, 'uploads')
 
-app.post('/profile', upload.single('avatar'), function (req, res, next) {
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, UPLOAD_PATH)
+  }
+})
+
+const upload = multer({ 
+  storage,
+  fileFilter: function(req, file, cb) {
+
+    // The function should call `cb` with a boolean
+    // to indicate if the file should be accepted
+  
+    // To reject this file pass `false`, like so:
+
+    // req.fileValidationError = "Forbidden extension";
+    // return cb(null, false, req.fileValidationError)
+  
+    // To accept the file pass `true`, like so:
+    // cb(null, true)
+  
+  }
+})
+
+app.post('/upload', upload.single('file'), function(req, res){
   // req.file is the `avatar` file
   // req.body will hold the text fields, if there were any
-  res.send({status: 'ok'})
+  if (req.fileValidationError) {
+    return res.status(400).send({error: req.fileValidationError})
+  }
+  // Everything went fine.
+  res.status(200).send({status: 'ok', filename: req.file.filename})
+})
+
+app.get('/image/:id', function(req, res){
+  // Validate that req.params.id is 16 bytes hex string
+  if (/^[0-9A-F]{32}$/i.test(req.params.id)) {
+    const filePath = path.join(UPLOAD_PATH, req.params.id);
+    let buffer
+    try {
+      buffer = readChunk.sync(filePath, 0, 4100);
+    } catch (error) {
+      return res.status(400).send({error: 'Not found'})
+    }
+    // Get the stored image type for this image
+    const storedMimeType = fileType(buffer);
+    res.setHeader('Content-Type', storedMimeType.mime);
+    fs.createReadStream(filePath).pipe(res);
+  } else {
+    res.status(400).send({error: 'Invalid img url'});
+  }
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send({error: 'Unexpected error'})
 })
 
 app.get('/', (req, res) => {
@@ -20,8 +76,8 @@ app.get('/', (req, res) => {
   </head>
   
   <body>
-    <form action="/profile" method="post" enctype="multipart/form-data">
-      <input type="file" name="avatar" />
+    <form action="/upload" method="post" enctype="multipart/form-data">
+      <input type="file" name="file" />
       <br />
       <button type="submit" >Submit</button>
     </form>
